25c8112347c3bc502b0bfa993295e595f92ee511
Владимир Кучин <vovjak@gmail.com>
2023-08-02 23:47:19 +0300

    Задание 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
+
 function compareArrays(arr1, arr2) {
-  return arr1.lenght === arr2.length && 
+
+  return (arr1.length === arr2.length) && arr1.every((element, index) => element === arr2);
+
 }
 
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, filteredUsersAges) => acc + item / filteredUsersAges.length, 0);
 }
