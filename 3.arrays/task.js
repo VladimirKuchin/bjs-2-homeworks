c27b82c5bb0e030adc520af98416d7d152949ee3
Владимир Кучин <vovjak@gmail.com>
2023-08-03 01:25:04 +0300

    Задание 3 полностью

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 
 function compareArrays(arr1, arr2) {
 
-  return (arr1.length === arr2.length) && arr1.every((element, index) => element === arr2);
+  return (arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index]);
 
 }
 
