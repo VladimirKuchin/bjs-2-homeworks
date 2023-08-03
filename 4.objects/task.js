667b104fc1257ad74b461a96b06a4774e5492bff
Владимир Кучин <vovjak@gmail.com>
2023-08-04 00:05:47 +0300

    Задание 4 Готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,41 @@
+let StudentFirst = new Student("Вероника", "женский", 21);
+let StudentSecond = new Student("Анна", "женский", 20);
+let StudentThird = new Student("Максим", "мужской", 19);
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (typeof this.marks === false || this.marks === undefined) {
+    return 0;
+  }
+  this.marks = [...this.marks, ...marksToAdd];
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (typeof this.marks === false || this.marks === undefined) {
+      return 0;
+  }
+  let result = this.marks.reduce((acc, item, index, arr) => {
+  	acc += item;
+  	if (index === arr.length - 1) {
+  		return acc / arr.length
+  	} return acc;
+  }, 0);
+  return result;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
 }
